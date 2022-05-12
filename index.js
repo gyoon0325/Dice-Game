a75400b7005668cd60f857d701c9a34cd6c7f652
gyoon0325 <103936969+gyoon0325@users.noreply.github.com>
2022-05-12 10:23:48 +1000

    Updating mistake on line 23
    else if statement had incorrect case randomnumber 2> randomnumber 2

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 if (randomNumber1 > randomNumber2) {
   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
 }
-else if (randomNumber2 > randomNumber2) {
+else if (randomNumber2 > randomNumber1) {
   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
 }
 else {
